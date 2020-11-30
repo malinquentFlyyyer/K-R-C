c9b11d472baf418357ac5a384c4a4b4c0cb10f93
Tom Joseph <tomjosephx3@gmail.com>
2020-11-30 17:30:05 -0500

    Completed sections 1.4 and 1.5

--- chapter1/float-arithmetic.c
+++ chapter1/float-arithmetic.c
@@ -4,14 +4,14 @@
     for fahr = 0, 20, ..., 300; floating-point version */
 
 int main()
-{ 
+{   
     float fahr, celsius;
     float lower, upper, step;
 
     lower = 0; /* lower limit of temperature scale */
     upper = 300; /* upper limit */
     step = 20; /* step size */
-
+    
     fahr = lower;
     while (fahr <= upper)
     {
